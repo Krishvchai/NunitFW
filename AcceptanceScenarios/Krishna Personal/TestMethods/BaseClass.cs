72cc205812d3e23323d5fc1eae8c7567bee56922
Krishna Chaitanya Vempati <KrishnaChaitanya.Vempati@thomsonreuters.com>
2024-03-15 15:17:02 +0530

    Changes

---
+++ AcceptanceScenarios/Krishna Personal/TestMethods/BaseClass.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krishna_Personal.TestMethods
+{
+    class BaseClass
+    {
+        public static IWebDriver GetWebDriver;
+        protected EngagementContext EngagementContext { get; set; }
+        [OneTimeSetUp]
+        public void BeforeSuite()
+        {
+            //ToDo
+        }
+        [SetUp]
+        public void BeforeTest()
+        {
+            GetWebDriver = new ChromeDriver();
+            EngagementContext.Driver = GetWebDriver;
+            GetWebDriver.Manage().Window.Maximize();
+            GetWebDriver.Navigate().GoToUrl("");
+        }
+        [TearDown]
+        public void GetResult()
+        {
+
+        }
+
+        [OneTimeTearDown]
+        public static void OneTimeDispose()
+        {
+            
+        }
+    }
+}
---
+++ AcceptanceScenarios/Krishna Personal/TestMethods/EngagementContext.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krishna_Personal.TestMethods
+{
+    public class EngagementContext
+    {
+        public IWebDriver Driver { get; set; }
+    }
+}
